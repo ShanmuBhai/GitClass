6ae8454e79f7277e6428526e2d27f98477a92653
ShanmuBhai <sanmu0052gmail.com>
2022-03-16 00:32:20 +0530

    Welcome

---
+++ src/test/java/com/git/GitClass1.java
@@ -0,0 +1,25 @@
+package com.git;
+
+public class GitClass1 {
+
+	public void method1() {
+		System.out.println("method 11");
+	}
+
+	public void method2() {
+		System.out.println("method 12");
+	}
+
+	public void method3() {
+		System.out.println("method 13");
+	}
+	
+	public static void main(String[] args) {
+		GitClass1 class1 = new GitClass1();
+		class1.method1();
+		class1.method2();
+		class1.method3();
+		
+	}
+
+}
